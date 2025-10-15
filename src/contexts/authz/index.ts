0772c03b8ecf6e697636f8080c827183fbdade76
NhatBangLe <bangle37568@gmail.com>
2025-10-15 21:15:53 +0700

    implement AuthzContext and Guard component

--- src/contexts/authz/index.ts
+++ src/contexts/authz/index.ts
@@ -6,11 +6,22 @@ import type {
 } from '@auth0/auth0-react';
 import { createContext, useContext } from 'react';
 
-export type Role = 'ADMIN' | 'READER';
+export type PermissionKey =
+	| 'READ_GENRE'
+	| 'CREATE_GENRE'
+	| 'UPDATE_GENRE'
+	| 'DELETE_GENRE';
+
+export const PermissionEnum: Record<PermissionKey, string> = {
+	READ_GENRE: 'read:genre',
+	CREATE_GENRE: 'create:genre',
+	UPDATE_GENRE: 'update:genre',
+	DELETE_GENRE: 'delete:genre',
+};
 
 export interface AuthenticatedUser extends Omit<User, 'sub'> {
 	id: string;
-	roles: Role[];
+	permissions: PermissionKey[];
 }
 
 export interface AuthzData {
@@ -18,7 +29,7 @@ export interface AuthzData {
 	isLoading: boolean;
 	isAuthenticated: boolean;
 	loginWithRedirect: (
-		options?: RedirectLoginOptions<AppState>,
+		options?: RedirectLoginOptions<AppState>
 	) => Promise<void>;
 	logout: (options?: LogoutOptions) => Promise<void>;
 }
@@ -29,4 +40,5 @@ export const useAuthz = () => {
 	const ctx = useContext(AuthzContext);
 	if (ctx === undefined)
 		throw new Error('useAuthz must be used within AuthzProvider');
+	return ctx;
 };
--- src/contexts/authz/provider.tsx
+++ src/contexts/authz/provider.tsx
@@ -1,6 +1,11 @@
 import { useAuth0 } from '@auth0/auth0-react';
-import { AuthzContext, type AuthzData } from '.';
-import { useMemo, type PropsWithChildren } from 'react';
+import {
+	AuthzContext,
+	PermissionEnum,
+	type AuthzData,
+	type PermissionKey,
+} from '.';
+import { useEffect, useState, type PropsWithChildren } from 'react';
 import * as jwt from 'jsonwebtoken';
 
 const AuthzProvider = ({ children }: PropsWithChildren) => {
@@ -12,20 +17,50 @@ const AuthzProvider = ({ children }: PropsWithChildren) => {
 		logout,
 		getAccessTokenSilently,
 	} = useAuth0();
-	const state = useMemo<AuthzData>(() => {
-		let currentUser: AuthzData['user'] = undefined;
-		if (isAuthenticated) {
-			jwt.decode();
-		}
+	const [state, setState] = useState<AuthzData>({
+		isLoading: isLoading,
+		isAuthenticated: isAuthenticated,
+		loginWithRedirect: loginWithRedirect,
+		logout: logout,
+	});
+	useEffect(() => {
+		if (!isAuthenticated) return;
+		getAccessTokenSilently()
+			.then(async (token) => {
+				const currentUser = user!;
+				if (currentUser.sub === undefined)
+					throw new Error('Token error: No subject claim in access token');
 
-		return {
-			user: currentUser,
-			isLoading: isLoading,
-			isAuthenticated: isAuthenticated,
-			loginWithRedirect: loginWithRedirect,
-			logout: logout,
-		};
-	}, []);
+				const payload = jwt.decode(token, { json: true });
+				if (!payload)
+					throw new Error(
+						'Token error: Cannot retrieve payload from access token.'
+					);
+
+				const permissions = (currentUser['roles'] as string[])
+					.map((value) => {
+						const entry = Object.entries(PermissionEnum).find(
+							([, v]) => v === value
+						);
+						if (entry === undefined) return null;
+
+						return entry[0] as PermissionKey;
+					})
+					.filter((permission) => permission !== null);
+				setState((pre) => ({
+					...pre,
+					user: {
+						id: currentUser.sub!,
+						...currentUser,
+						permissions,
+					},
+				}));
+			})
+			.catch(async (err) => {
+				console.error(err);
+				await logout();
+			});
+	}, [getAccessTokenSilently, isAuthenticated, logout, user]);
 
 	return (
 		<AuthzContext.Provider value={state}>{children}</AuthzContext.Provider>
