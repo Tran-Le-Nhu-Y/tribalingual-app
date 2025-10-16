6ca894284bafb5c7d4af917292f17567a930fe49
NhatBangLe <bangle37568@gmail.com>
2025-10-17 06:03:12 +0700

    fix read undefined property

--- src/util/index.ts
+++ src/util/index.ts
@@ -117,7 +117,7 @@ export const axiosBaseQuery =
 			const err = axiosError as AxiosError;
 			return {
 				error: {
-					status: err.response!.status || 500,
+					status: err.response?.status || 500,
 					data: err.response?.data || err.message,
 				},
 			};
